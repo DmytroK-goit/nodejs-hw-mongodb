7db7d65aa929c47b4fdbe3e3a9393f20eba0f3b5
Dmytro Kovbasiuk <159040488+DmytroK-goit@users.noreply.github.com>
2024-12-31 20:36:21 +0200

    credentisls

--- src/server.js
+++ src/server.js
@@ -13,7 +13,12 @@ const app = express();
 
 app.use(cookieParser());
 app.use('/photos', express.static(path.resolve('src/public/photo')));
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://nodejs-hw-mongodb-2hns.onrender.com/',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use('/api-docs', swaggerDocs());
 
